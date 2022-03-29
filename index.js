5c1e40c9002d7ced3ce0a851ec887c65903c4105
suuru <97366487+suuru@users.noreply.github.com>
2022-03-29 18:25:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0]
+
+image1.setAttribute('src', randomImageSource);
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1].setAttribute('src', randomImageSource2);
+
+var player1 = prompt("What is your player 1 name?");
+
+var player2 = prompt("What is your player 2 name?");
+
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML =   player1  + ' Wins!! ';
+}
+
+
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML =    player2  + ' Wins!! ';
+}
+
+else{
+  document.querySelector("h1").innerHTML = "Draw!"
+}
